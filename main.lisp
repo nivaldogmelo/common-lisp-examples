3feb221d66e95323b98d0e47b5c8211cbe05c417
nivaldogmelo <nivaldogmelo@gmail.com>
2019-06-06 23:17:45 -0300

    Add instructions to main functions

--- main.lisp
+++ main.lisp
@@ -1,24 +1,33 @@
 (defvar *test-name* nil)
 
-(defmacro check (&body forms)
-  `(combine-results
-     ,@(loop for f in forms collect `(report-result ,f ',f))))
-
-
 (defmacro with-gensyms ((&rest names) &body body)
   `(let ,(loop for n in names collect `(,n (gensym)))
      ,@body))
 
+(defmacro check (&body forms)
+  "Run each expresssion in 'forms' as a test case."
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
 (defmacro combine-results (&body forms)
+  "Combine the results (as booleans) of evaluating 'forms' in order"
   (with-gensyms (result)
     `(let ((,result t))
        ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
        ,result)))
 
 (defmacro deftest (name parameters &body body)
+  "Define a test function. Within a test function we can call
+other test functions or use 'check' to run individual test
+cases."
   `(defun ,name ,parameters
      (let ((*test-name* (append *test-name* (list ',name))))
        ,@body)))
+
+(defun report-result (result form)
+  "Report the results of a single test case. Called by 'check'."
+  (format t "~:[FAIL~;pass~] ...~a: ~a~%" result *test-name* form)
+  result)
 
 (deftest test-+ ()
     (check
@@ -38,7 +47,3 @@
 
 (deftest test-math ()
   (test-arithmetic))
-
-(defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ...~a: ~a~%" result *test-name* form)
-  result)
