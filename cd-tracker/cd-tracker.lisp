3e3d71af64e335bb68fdd86460257a1629f411e9
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-01-28 00:47:07 -0300

    Add comments to describe functions

--- cd-tracker/cd-tracker.lisp
+++ cd-tracker/cd-tracker.lisp
@@ -1,14 +1,19 @@
+;; Create CD
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
 
+;; Global variable to save CD's
 (defvar *db* nil)
 
+;; Add new records to DB variable
 (defun add-record (cd) (push cd *db*))
 
-(defun dumb-db ()
+;; Print CD's in a readable format
+(defun dump-db ()
   (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd)))
 
+;; Read CD's info interactively
 (defun prompt-read (prompt)
   (format *query-io* "~a: " prompt)
   (force-output *query-io*)
@@ -19,47 +24,50 @@
    (prompt-read "Title")
    (prompt-read "Artist")
    (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
-   (y-or-n-p "Ripped [y/n]")))
+   (y-or-n-p "Ripped [y/n]: ")))
 
 (defun add-cds ()
   (loop (add-record (prompt-for-cd))
      (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
 
+;; Save DB variable
 (defun save-db (filename)
   (with-open-file (out filename
-		       :direction :output
-		       :if-exists :supersede)
+                       :direction :output
+                       :if-exists :supersede)
     (with-standard-io-syntax
       (print *db* out))))
 
+;; Load DB variable
 (defun load-db (filename)
   (with-open-file (in filename)
     (with-standard-io-syntax
       (setf *db* (read in)))))
 
-(defun select (selector-fn)
-  (remove-if-not selector-fn *db*))
-
+;; Query DB
 (defun make-comparison-expr (field value)
   `(equal (getf cd ,field) ,value))
 
-(defun make-comparisons-list (fields)
+(defun make-comparison-list (fields)
   (loop while fields
-       collecting (make-comparison-expr (pop fields) (pop fields))))
+     collecting (make-comparison-expr (pop fields) (pop fields))))
 
 (defmacro where (&rest clauses)
-  `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
+  `#'(lambda (cd) (and ,@(make-comparison-list clauses))))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
 
 (defun update (selector-fn &key title artist rating (ripped nil ripped-p))
   (setf *db*
-	(mapcar
-	 #'(lambda (row)
-	     (when (funcall selector-fn row)
-	       (if title    (setf (getf row :title) title))
-	       (if artist   (setf (getf row :artist) artist))
-	       (if rating   (setf (getf row :rating) rating))
-	       (if ripped-p (setf (getf row :ripped) ripped)))
-	     row) *db*)))
+        (mapcar
+         #'(lambda (row)
+             (when (funcall selector-fn row)
+                            (if title    (setf (getf row :title)  title))
+                            (if artist   (setf (getf row :artist) artist))
+                            (if rating   (setf (getf row :rating) rating))
+                            (if ripped-p (setf (getf row :ripped) ripped)))
+               row) *db*)))
 
 (defun delete-rows (selector-fn)
   (setf *db* (remove-if selector-fn *db*)))
